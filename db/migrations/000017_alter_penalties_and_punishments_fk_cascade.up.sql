e357a975f0cc787aba167da00a10fa549b22da57
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2026-02-21 10:00:37 +0100

    chore: set 'ON DELETE CASCADE' for types

---
+++ db/migrations/000017_alter_penalties_and_punishments_fk_cascade.up.sql
@@ -0,0 +1,17 @@
+ALTER TABLE penalties
+DROP CONSTRAINT IF EXISTS penalties_penalty_type_id_fkey;
+
+ALTER TABLE penalties
+ADD CONSTRAINT penalties_penalty_type_id_fkey
+FOREIGN KEY (penalty_type_id)
+REFERENCES penalty_types(id)
+ON DELETE CASCADE;
+
+ALTER TABLE punishments
+DROP CONSTRAINT IF EXISTS punishments_punishment_type_id_fkey;
+
+ALTER TABLE punishments
+ADD CONSTRAINT punishments_punishment_type_id_fkey
+FOREIGN KEY (punishment_type_id)
+REFERENCES punishment_types(id)
+ON DELETE CASCADE;
